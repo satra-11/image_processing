3b4458cf0d61efaa38a530057efbffa39f558a9a
satra-11 <syuzuki546@gmail.com>
2023-09-03 16:16:18 +0900

    マスキング

---
+++ masking.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("bird.jpg")
+
+mask = cv2.imread("assets/mask.png")
+
+mask = cv2.resize(mask, (img.shape[1], img.shape[0]))
+
+mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+
+my_img = cv2.bitwise_and(img, img, mask=mask)
+
+cv2.imshow("sample", my_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+'''
+マスキングでは、白黒のチャンネル1の画像を用いる。
+'''
